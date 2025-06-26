ee414daa1a64a85a8e9e2aedf98a77bd4df3825b
Godwin0788 <gmwachan@gmail.com>
2025-06-26 08:45:26 -0800

    arithe

--- variables.js
+++ variables.js
@@ -6,3 +6,50 @@ let w=4
 let q=6
 let p=w+q
 console.log(p)
+var t=24
+var y=3
+var z=t+y
+console.log(z)
+// creating a variable you use
+let m=10
+console.log(m)
+// re assigning m
+m=8
+console.log(m)
+// creating a variable using const e.g age, nationality, 
+const h=11
+// h=11 cannot re assign a const variable
+console.log(h)
+const nationality="Uganda"
+const gender="Male"
+const myDob="28/05/1995"
+const myFname="Mwachan"
+const religion="catholic"
+console.log(religion)
+console.log(nationality)
+console.log(gender)
+console.log(myDob)
+console.log(myFname)
+// arithematic in variables
+let n=4+5
+let o="Mwachan"+"Godwin"
+console.log(o)
+console.log(n)
+let f="4"+"5"
+console.log(f)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
