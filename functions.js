1df05d8e7dd4a9aa3345213014610a98aedb3048
Godwin0788 <gmwachan@gmail.com>
2025-07-01 15:10:13 -0800

    functions

---
+++ functions.js
@@ -0,0 +1,39 @@
+function myName() {
+    console.log ("i come from luwero")
+}
+myName()
+
+function myMother() {
+    console.log ("My mother is walking")
+}
+myMother()
+function myMic(y1,y2) {
+    console.log(y1*y2)
+}
+myMic(4,10)
+function myText(x1,x2) {
+    console.log(x1+x2)
+}
+myText(6,8)
+
+// function to calculate the area of a rectangle
+function calculateAreaRectangle(length,width){
+    console.log(length*width)
+}
+calculateAreaRectangle(7,9)
+calculateAreaRectangle(5,8)
+// function to calculate the area of a trianagle 
+function calculateAreaTriangle(base,height){
+    console.log(base*height)
+    let area=1/2*base*height
+    console.log(area)
+}
+calculateAreaTriangle(3,9)
+// function to convert celsius to fahrenheit
+function convertCelsiusFahrenheit(celsius){
+    console.log(celsius)
+    let fahrenheit=celsius*9/5+32
+    console.log(fahrenheit)
+}
+convertCelsiusFahrenheit(2)
+convertCelsiusFahrenheit(37)
